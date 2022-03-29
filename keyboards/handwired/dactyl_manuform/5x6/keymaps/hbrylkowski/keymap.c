cd06bf64eb142555b6db769c792ec81dc49c1357
Hubert Bryłkowski <hubert.brylkowski@brainly.com>
2022-03-29 13:08:56 +0200

    added my keymap

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/hbrylkowski/keymap.c
@@ -0,0 +1,61 @@
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+#define MOD1_C LSA_T(KC_C)
+#define MOD2_V MEH_T(KC_V)
+
+// Right-hand home row mods
+#define CTL_J RCTL_T(KC_J)
+#define SFT_K RSFT_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define GUI_SCLN RGUI_T(KC_SCLN)
+#define M1_COM RSA_T(KC_COMM)
+#define M2_DOT MEH_T(KC_DOT)
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT_5x6(
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+        KC_LSFT, CTL_A , ALT_S , GUI_D , SFT_F , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LCTL, KC_Z  , KC_X  , MOD1_C, MOD2_V, KC_B  ,                         KC_N  , KC_M  ,M1_COM ,M2_DOT ,KC_SLSH,KC_BSLASH,
+                         KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
+                                         RAISE,KC_SPC,                        KC_ENT, LOWER,
+                                         KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
+                                         KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+    ),
+
+    [_LOWER] = LAYOUT_5x6(
+        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+        _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                                                _______,KC_PSCR,            _______, KC_P0,
+                                                _______,_______,            _______,_______,
+                                                _______,_______,            _______,_______,
+                                                _______,_______,            _______,_______
+
+    ),
+
+    [_RAISE] = LAYOUT_5x6(
+          KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+          _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
+          _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+          _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+                                                  _______,_______,            KC_EQL ,_______,
+                                                  _______,_______,            _______,_______,
+                                                  _______,_______,            _______,_______,
+                                                  _______,_______,            _______,_______
+    )
+};
